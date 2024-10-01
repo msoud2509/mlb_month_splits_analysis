e98250d9c1c87e8f5ea953ece567a06b074d90be
msoud2509 <msouders06@gmail.com>
2024-10-01 10:50:48 -0500

    update formatting

--- mlb_month_splits/get_game_logs.py
+++ mlb_month_splits/get_game_logs.py
@@ -4,23 +4,23 @@ import pybaseball
 
 
 def get_raw_logs(year):
+    """gets regular season game logs of every team by year and writes to respective files"""
     #make sure directory exists beforehand
-    #correct path: final_files/hittingteamgamelogs/teamgamelogs{year}/hitting/
-    teamabbrevdataf = pd.read_csv('final_files/team_abbreviations.csv')
-    for team in teamabbrevdataf['Abbreviations']:
+    #correct path: mlb_month_splits/hittingteamgamelogs/teamgamelogs{year}/hitting/
+    teamabbrevdataf = pd.read_csv("mlb_month_splits/team_abbreviations.csv")
+    for team in teamabbrevdataf["Abbreviations"]:
         #pybaseball.team_game_logs defaults to hitting
         df = pybaseball.team_game_logs(year,team)
-        df.to_csv('final_files/hittingteamgamelogs/teamgamelogs{}/hitting/{}hit{}.csv'.format(year,year,team),index=False)
+        df.to_csv("mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting/{}hit{}.csv".format(year,year,team),index=False)
 
 
 def add_game_by_game_stats(year):
-    #need to get daily stats for gamelog files, because baseball-reference lists accumulation of stats over season per entry
-    gameloghitlist = os.listdir('final_files/hittingteamgamelogs/teamgamelogs{}/hitting'.format(year))
+    """need to get daily stats for gamelog files, because baseball-reference lists accumulation of stats over season per entry"""
+    gameloghitlist = os.listdir("mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting".format(year))
     for file in gameloghitlist:
-        dataf = pd.read_csv('final_files/hittingteamgamelogs/teamgamelogs{}/hitting/{}'.format(year,file))
-        dataf['gameBA'] = dataf['H'] / dataf['AB']
-        dataf['gameOBP'] = (dataf['H'] + dataf['BB'] + dataf['HBP'])/dataf['PA']
-        dataf['gameSLG'] = ((4*dataf['HR']) + (3*dataf['3B']) + (2*dataf['2B']) + (dataf['H']-dataf['3B']-dataf['2B']-dataf['HR']))/dataf['AB']
-        dataf['gameOPS'] = (dataf['gameOBP'] + dataf['gameSLG'])
-        dataf.to_csv('final_files/hittingteamgamelogs/teamgamelogs{}/hitting/{}'.format(year,file), index=False)
-
+        dataf = pd.read_csv("mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting/{}".format(year,file))
+        dataf["gameBA"] = dataf["H"] / dataf["AB"]
+        dataf["gameOBP"] = (dataf["H"] + dataf["BB"] + dataf["HBP"])/dataf["PA"]
+        dataf["gameSLG"] = ((4*dataf["HR"]) + (3*dataf["3B"]) + (2*dataf["2B"]) + (dataf["H"]-dataf["3B"]-dataf["2B"]-dataf["HR"]))/dataf["AB"]
+        dataf["gameOPS"] = (dataf["gameOBP"] + dataf["gameSLG"])
+        dataf.to_csv("mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting/{}".format(year,file), index=False)
--- mlb_month_splits/get_months_battingsplits.py
+++ mlb_month_splits/get_months_battingsplits.py
@@ -2,34 +2,34 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import os
 
-'''#comment from pybaseball repo on the necessity of BRefSession
+"""#comment from pybaseball repo on the necessity of BRefSession
 
     Bref is needed because Baseball Reference has rules against bots.
 
     Current policy says no more than 20 requests per minute, but in testing
     anything more than 10 requests per minute gets you blocked for one hour.
 
-    So this global session will prevent a user from getting themselves blocked.'''
+    So this global session will prevent a user from getting themselves blocked."""
     
 from pybaseball.datasources.bref import BRefSession
 session = BRefSession()
 
 def gather_playoffs(year):
-    df = pd.DataFrame(columns=['Tm','Playoff_BA','Playoff_OPS','Playoff_SO','Playoff_AB','Playoff_Series_Count'])
+    """gets playoff hitting data from baseball reference (web scrape)"""
+    df = pd.DataFrame(columns=["Tm","Playoff_BA","Playoff_OPS","Playoff_SO","Playoff_AB","Playoff_Series_Count"])
     #only takes 2023 and 2022 soz
     if year == 2022 or year == 2023:
-        serieslist = ['WS','ALCS','NLCS','ALDS1','NLDS1','ALDS2','NLDS2','ALWC1','NLWC1','ALWC2','NLWC2']
-    #CHANGE HERE!!!!!!
+        serieslist = ["WS","ALCS","NLCS","ALDS1","NLDS1","ALDS2","NLDS2","ALWC1","NLWC1","ALWC2","NLWC2"]
     elif (year in range(2012,2022)) and year != 2020:
-        serieslist= ['WS','ALCS','NLCS','ALDS1','NLDS1','ALDS2','NLDS2','ALWC','NLWC']
+        serieslist= ["WS","ALCS","NLCS","ALDS1","NLDS1","ALDS2","NLDS2","ALWC","NLWC"]
     else:
-        raise Exception('only playoff formats from 2012 to present (excluding 2020) are accounted for at the moment')
+        raise Exception("only playoff formats from 2012 to present (excluding 2020) are accounted for at the moment")
     
     playoff_teams_list = []
 
-    teamabbrevdataf = pd.read_csv('mlb_month_splits/team_abbreviations.csv')
+    teamabbrevdataf = pd.read_csv("mlb_month_splits/team_abbreviations.csv")
     if year < 2022:
-        teamabbrevdataf.at[7, 'Team'] = 'Cleveland Indians'
+        teamabbrevdataf.at[7, "Team"] = "Cleveland Indians"
     
     
     for series in serieslist:
@@ -39,16 +39,16 @@ def gather_playoffs(year):
         soup = BeautifulSoup(content,"lxml")
 
         #head text contains full name of each team playing in series
-        head = soup.find('h1')
+        head = soup.find("h1")
         head = head.text.strip()
         count = 0
         teams = []
-        for c,x in enumerate(teamabbrevdataf['Team']):
+        for c,x in enumerate(teamabbrevdataf["Team"]):
             if x in head:
-                if teamabbrevdataf.iloc[c]['Abbreviations'] not in playoff_teams_list:
-                    playoff_teams_list.append(teamabbrevdataf.iloc[c]['Abbreviations'])
+                if teamabbrevdataf.iloc[c]["Abbreviations"] not in playoff_teams_list:
+                    playoff_teams_list.append(teamabbrevdataf.iloc[c]["Abbreviations"])
                 count += 1
-                teams.append(teamabbrevdataf.iloc[c]['Abbreviations'])
+                teams.append(teamabbrevdataf.iloc[c]["Abbreviations"])
             #only two teams can play in a series
             if count == 2:
                 break
@@ -59,27 +59,27 @@ def gather_playoffs(year):
             dataf = pd.read_html(str(table),header=[1])[0]
             
             #remove unecessary columns
-            dataf = dataf.drop(['G.1','AB.1','R.1','H.1','HR.1','RBI.1','SB.1','BA.1','OPS.1'],axis=1)
+            dataf = dataf.drop(["G.1","AB.1","R.1","H.1","HR.1","RBI.1","SB.1","BA.1","OPS.1"],axis=1)
             
 
-            if team not in df['Tm'].tolist():
+            if team not in df["Tm"].tolist():
                 row_to_add = [team, 0, 0, 0, 0, 0]
                 df.loc[len(df)] = row_to_add
             
             #add up totals
-            df.at[df['Tm'].tolist().index(team),'Playoff_BA'] = df.iloc[df['Tm'].tolist().index(team)]['Playoff_BA'] + float(dataf.iloc[-1]['BA'])
-            df.at[df['Tm'].tolist().index(team),'Playoff_OPS'] = df.iloc[df['Tm'].tolist().index(team)]['Playoff_OPS'] + float(dataf.iloc[-1]['OPS'])
-            df.at[df['Tm'].tolist().index(team),'Playoff_SO'] = df.iloc[df['Tm'].tolist().index(team)]['Playoff_SO'] + float(dataf.iloc[-1]['SO'])
-            df.at[df['Tm'].tolist().index(team),'Playoff_AB'] = df.iloc[df['Tm'].tolist().index(team)]['Playoff_AB'] + float(dataf.iloc[-1]['AB'])
-            df.at[df['Tm'].tolist().index(team),'Playoff_Series_Count'] = df.iloc[df['Tm'].tolist().index(team)]['Playoff_Series_Count'] + 1
+            df.at[df["Tm"].tolist().index(team),"Playoff_BA"] = df.iloc[df["Tm"].tolist().index(team)]["Playoff_BA"] + float(dataf.iloc[-1]["BA"])
+            df.at[df["Tm"].tolist().index(team),"Playoff_OPS"] = df.iloc[df["Tm"].tolist().index(team)]["Playoff_OPS"] + float(dataf.iloc[-1]["OPS"])
+            df.at[df["Tm"].tolist().index(team),"Playoff_SO"] = df.iloc[df["Tm"].tolist().index(team)]["Playoff_SO"] + float(dataf.iloc[-1]["SO"])
+            df.at[df["Tm"].tolist().index(team),"Playoff_AB"] = df.iloc[df["Tm"].tolist().index(team)]["Playoff_AB"] + float(dataf.iloc[-1]["AB"])
+            df.at[df["Tm"].tolist().index(team),"Playoff_Series_Count"] = df.iloc[df["Tm"].tolist().index(team)]["Playoff_Series_Count"] + 1
 
         #for checkpoints for each playoff series when loading data, use this code:
-        print('series done')
+        #print("series done")
 
     #get averages per series
-    df['Playoff_BA'] = df['Playoff_BA']/df['Playoff_Series_Count']
-    df['Playoff_OPS'] = df['Playoff_OPS']/df['Playoff_Series_Count']
-    df['Playoff_SO/BA'] = df['Playoff_SO'] / df['Playoff_AB']
+    df["Playoff_BA"] = df["Playoff_BA"]/df["Playoff_Series_Count"]
+    df["Playoff_OPS"] = df["Playoff_OPS"]/df["Playoff_Series_Count"]
+    df["Playoff_SO/BA"] = df["Playoff_SO"] / df["Playoff_AB"]
     
     if year == 2022 or year == 2023:
         try:
@@ -95,17 +95,18 @@ def gather_playoffs(year):
     #returns playoff_teams_list to gather regular season data
     return df, playoff_teams_list
 
-#adds regular season data to existing pandas dataframe
+
 def add_regszn_data(df, year, playoff_teams):
+    """adds regular season data to existing playoff pandas dataframe to be used for analysis"""
     #create all the new necessary columns
-    month_splits_list = ['Mar/Apr_BA','Mar/Apr_OPS','Mar/Apr_SO/PA','Mar/Apr_LOB','May_BA','May_OPS','May_SO/PA','May_LOB',
-    'Jun_BA','Jun_OPS','Jun_SO/PA','Jun_LOB','Jul_BA','Jul_OPS','Jul_SO/PA','Jul_LOB','Aug_BA','Aug_OPS','Aug_SO/PA','Aug_LOB',
-    'Sep/Oct_BA','Sep/Oct_OPS','Sep/Oct_SO/PA','Sep/Oct_LOB']
+    month_splits_list = ["Mar/Apr_BA","Mar/Apr_OPS","Mar/Apr_SO/PA","Mar/Apr_LOB","May_BA","May_OPS","May_SO/PA","May_LOB",
+    "Jun_BA","Jun_OPS","Jun_SO/PA","Jun_LOB","Jul_BA","Jul_OPS","Jul_SO/PA","Jul_LOB","Aug_BA","Aug_OPS","Aug_SO/PA","Aug_LOB",
+    "Sep/Oct_BA","Sep/Oct_OPS","Sep/Oct_SO/PA","Sep/Oct_LOB"]
     
     df[month_splits_list] = 0
 
-    gameloghitlist = os.listdir('mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting'.format(year))
-    monthlist = ['Apr','May','Jun','Jul','Aug','Sep']
+    gameloghitlist = os.listdir("mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting".format(year))
+    monthlist = ["Apr","May","Jun","Jul","Aug","Sep"]
     
     #sort to make searching in gameloghitlist easier
     playoff_teams.sort()
@@ -120,35 +121,31 @@ def add_regszn_data(df, year, playoff_teams):
         while(team not in gameloghitlist[hitlogcount]):
             hitlogcount += 1
         row_to_add = []
-        teamgamelogdf = pd.read_csv('mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting/{}hit{}.csv'.format(year, year, team))
+        teamgamelogdf = pd.read_csv("mlb_month_splits/hittingteamgamelogs/teamgamelogs{}/hitting/{}hit{}.csv".format(year, year, team))
         
         #adds month stats for one team a month at a time
         for month in monthlist:
             #combine april and march games, as well as october and september games for simplicity
-            if month == 'Apr':
-                splitdataf = teamgamelogdf.loc[teamgamelogdf['Date'].str.contains(month) | teamgamelogdf['Date'].str.contains('Mar')]
-            elif month == 'Sep':
-                splitdataf = teamgamelogdf.loc[teamgamelogdf['Date'].str.contains(month) | teamgamelogdf['Date'].str.contains('Oct')]
+            if month == "Apr":
+                splitdataf = teamgamelogdf.loc[teamgamelogdf["Date"].str.contains(month) | teamgamelogdf["Date"].str.contains("Mar")]
+            elif month == "Sep":
+                splitdataf = teamgamelogdf.loc[teamgamelogdf["Date"].str.contains(month) | teamgamelogdf["Date"].str.contains("Oct")]
             else:
-                splitdataf = teamgamelogdf.loc[teamgamelogdf['Date'].str.contains(month)]
+                splitdataf = teamgamelogdf.loc[teamgamelogdf["Date"].str.contains(month)]
 
-            row_to_add.append(avg(splitdataf['gameBA']))
-            avgOBP = avg(splitdataf['gameOBP'])
-            avgSLG = avg(splitdataf['gameSLG'])
+            row_to_add.append(avg(splitdataf["gameBA"]))
+            avgOBP = avg(splitdataf["gameOBP"])
+            avgSLG = avg(splitdataf["gameSLG"])
             #appends ops
             row_to_add.append(avgOBP + avgSLG)
             
-            row_to_add.append(avg(splitdataf['SO'])/avg(splitdataf['PA']))
-            row_to_add.append(avg(splitdataf['LOB']))
+            row_to_add.append(avg(splitdataf["SO"])/avg(splitdataf["PA"]))
+            row_to_add.append(avg(splitdataf["LOB"]))
 
         #adds all month stats of one team to existing dataframe
-        df.loc[df['Tm'].tolist().index(team), month_splits_list] = row_to_add
+        df.loc[df["Tm"].tolist().index(team), month_splits_list] = row_to_add
 
         #for checkpoints for each playoff series when loading data, use this code:
-        print('team done')
+        #print("team done")
     
-    return df
-
-            
-            
-            
+    return df           
